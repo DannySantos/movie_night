42fbb4252bf082dc9c2941a4314efc1f1bab0e2a
Danny Santos <danny@ethicalswitch.com>
2016-05-14 16:15:41 +0100

    relationships

--- db/migrate/4_create_votes.rb
+++ db/migrate/4_create_votes.rb
@@ -1,5 +1,9 @@
 class CreateVotes < ActiveRecord::Migration
   def change
-    
+    create table :votes do |t|
+      t.references :poll, index: true, foreign_key: true
+      t.references :user, index: true, foreign_key: true
+      t.references :movie, index: true, foreign_key: true
+    end
   end
 end
--- movie.rb
+++ movie.rb
@@ -1,3 +1,4 @@
-class Movies < ActiveRecord::Base
+class Movie < ActiveRecord::Base
+  belongs_to :suggester, class_name: "User", foreign_key: :suggester_id, inverse_of: :suggested_movies
   has_many :votes
 end
