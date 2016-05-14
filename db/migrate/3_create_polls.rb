82e30a370b14c11edde993fe9f9914981d960903
tamlyn10 <tamlyn@ethicalswitch.com>
2016-05-14 14:56:22 +0100

    t not p in create_polls

--- db/migrate/3_create_polls.rb
+++ db/migrate/3_create_polls.rb
@@ -3,6 +3,7 @@ class CreatePolls < ActiveRecord::Base
     create_table :polls do |t|
       t.string :name
       t.boolean :is_decided
-      t.date :date    
+      t.date :date
+    end    
   end
 end
